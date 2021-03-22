167fd406524153e84b73e26ccee039d5f9c8464c
Patrick Nascimento <patricknro@gmail.com>
2021-03-22 10:24:06 -0300

    Faz gravação e leitura utilizando o mongodb.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# mongodb
-Gravar e Fazer leitura dos dados (usando Docker)
+# MongoDB
+Gravar e fazer leitura dos dados (usando Docker)
+
+
+### Link do Tutorial
+- https://docs.microsoft.com/pt-br/aspnet/core/tutorials/first-mongo-app?view=aspnetcore-3.1&tabs=visual-studio
+
+### Instalar o Nuget Package abaixo (MongoDB.Driver)
+- https://www.nuget.org/packages/MongoDB.Driver/
+
+### Imagem do Docker utilizado
+- `> docker run --name <nome_container> -p <porta_interna_container>:<porta_externa> -d mongo:<versao>`
+
+*_A porta interna e externa utilizada foi 27017_
+
+### Aplicação para gerenciar o MongoDB foi utilizado o abaixo.
+- Robo 3T 1.4.3 (Build 48f7dfde - 3/2021)
+
+
+#cURL
+
+### Gravar log
+```
+curl --location --request POST 'http://localhost:63350/api/log/gravar' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    
+    "bookName": "Teste",
+    "price": 99.99,
+    "category": "Beans",
+    "author": "Patrick Nascimento"
+}'
+```
+
+### Ler o log
+```
+curl --location --request GET 'http://localhost:63350/api/log/obter-log/60580686a37ad0aa92f94da5'
+```
---
+++ UsandoMongoDb.Api/Controllers/LogController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsandoMongoDb.Api.Model;
+using UsandoMongoDb.Api.Service;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UsandoMongoDb.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogController : ControllerBase
+    {
+
+        private readonly BookService _bookService;
+
+        public LogController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        // GET: api/<LogController>
+        [HttpPost("gravar")]
+        public IActionResult Gravar(Book book)
+        {
+            _bookService.Create(book);
+
+            //return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+            return Ok();
+        }
+
+        // GET api/<LogController>/5
+        [HttpGet("obter-log/{id:length(24)}")]
+        public ActionResult<Book> ObterLogPorId(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        
+        [HttpGet("obter-todos-log")]
+        public IActionResult ObterTodosLog()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("remover/{id:length(24)}")]
+        public IActionResult RemoverLog(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book.Id);
+
+            return NoContent();
+        }
+
+    }
+}
